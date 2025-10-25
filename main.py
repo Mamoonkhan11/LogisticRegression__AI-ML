0ee115eafa2cf20be2b4741fab3e69efa885f9b1
Mamoon Khan <mamoonrashid5656@gmail.com>
2025-10-25 13:04:38 +0530

    update main file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from src.data_loader import Load_data
 from src.model import Train_model, Save_model
-from src.evaluate import evaluate_model
+from src.evaluate import Evaluate_model
 from src.utils import Plot_precision_recall_curve
 
 # 1. Load data
@@ -13,7 +13,7 @@ model = Train_model(X_train, y_train)
 Save_model(model)
 
 # 3. Evaluate model
-evaluate_model(model, X_test, y_test, threshold=0.5)
+Evaluate_model(model, X_test, y_test, threshold=0.5)
 
 # 4. Optional: Precision-Recall curve for threshold tuning
 y_probs = model.predict_proba(X_test)[:,1]
